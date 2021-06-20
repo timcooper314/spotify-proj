21b0326592e799f249ec779523f51b95e0cb46bb
Tim Cooper <timcooper314@gmail.com>
2021-06-20 11:51:43 +1000

    Created functions for computing cosine similarity matrix.

--- spotify_api.py
+++ spotify_api.py
@@ -2,21 +2,11 @@ import requests
 from pprint import pprint
 from collections import defaultdict
 import json
+import numpy as np
 
 
 class SpotifyClientAuthTokenExpiredException(Exception):
     pass
-
-
-def _filter_audio_features(spotify_data):
-    desired_fields = ['acousticness', 'danceability', 'energy', 'instrumentalness', 'liveness', 'speechiness', 'tempo']
-    additional_fields = ['key', 'loudness', 'mode', 'time_signature', 'valence']
-    return {field: spotify_data['audio_features'][0][field] for field in desired_fields}
-
-
-def check_api_response(response):
-    if 'error' in response:
-        raise SpotifyClientAuthTokenExpiredException(response['error']['message'])
 
 
 def get_auth_token():
@@ -24,6 +14,37 @@ def get_auth_token():
         token = f.readlines()
         f.close()
     return token[0]
+
+
+def check_api_response(response):
+    if 'error' in response:
+        raise SpotifyClientAuthTokenExpiredException(response['error']['message'])
+
+
+def _filter_audio_features(spotify_data):
+    desired_fields = ['acousticness', 'danceability', 'energy', 'instrumentalness', 'speechiness']
+    additional_fields = ['liveness', 'tempo', 'key', 'loudness', 'mode', 'time_signature', 'valence']
+    return {field: spotify_data['audio_features'][0][field] for field in desired_fields}
+
+
+def compute_cosine(v_1, v_2):
+    """Computes the cosine of the angle between vectors v_1 and v_2"""
+    return np.dot(v_1, v_2) / (np.linalg.norm(v_1)*np.linalg.norm(v_2))
+
+
+def compute_similarity_matrix(audio_features_array):
+    """Computes the (symmetrix, square) matrix of similarity values
+     between tracks, given an array of audio features vectors."""
+    num_tracks = len(audio_features_array)
+    similarity_matrix = np.eye(num_tracks)
+    # for i, vec in enumerate(audio_features_array):
+    for i in range(num_tracks):
+        v_i = audio_features_array[i]
+        for j in range(i+1, num_tracks):
+            v_j = audio_features_array[j]
+            similarity_matrix[i][j] = compute_cosine(v_i, v_j)
+    print(similarity_matrix)
+    return similarity_matrix
 
 
 class SpotifyClient:
@@ -72,12 +93,12 @@ class SpotifyClient:
         return response.json()
 
     def get_recently_played(self):
-        endpoint = 'player/recently-played/'
+        endpoint = 'me/player/recently-played/'
         spotify_data = self._get_api_data(endpoint)
         check_api_response(spotify_data)
         recently_played = defaultdict(list)
         for item in spotify_data['items']:
-            recently_played[item['track']['album']['artists'][0]['name']].append(item['track']['name'])
+            recently_played[item['track']['artists'][0]['name']].append(item['track']['name'])
         pprint(recently_played)
         return recently_played
 
@@ -127,9 +148,10 @@ class SpotifyClient:
         self.params['offset'] = offset
         spotify_data = self._get_api_data(endpoint)
         check_api_response(spotify_data)
+        pprint(spotify_data)
         top_tracks = defaultdict(list)
         for track_object in spotify_data['items']:
-            artist = track_object['track']['album']['artists'][0]['name']
+            artist = track_object['track']['artists'][0]['name']
             track = track_object['track']['name']
             top_tracks[artist].append(track)
         print(len(top_tracks))
@@ -140,14 +162,15 @@ class SpotifyClient:
         endpoint = "audio-features"
         self.params['ids'] = track_ids
         spotify_data = self._get_api_data(endpoint)
+        check_api_response(spotify_data)
         features = _filter_audio_features(spotify_data)
         return features
 
     def get_audio_features_of_currently_playing_track(self):
         """Requires OAuth token with scope user-read-currently-playing"""
         current_playing_data = self.get_current_playback()
-        artist = current_playing_data['item']['artists'][0]['name']
-        track = current_playing_data['item']['name']
+        # artist = current_playing_data['item']['artists'][0]['name']
+        # track = current_playing_data['item']['name']
         track_id = current_playing_data['item']['id']
         features = self.get_audio_features(track_id)
         pprint(features)
@@ -162,7 +185,7 @@ class SpotifyClient:
             features = self.get_audio_features(track_id)
             audio_features_vectors.append(list(features.values()))
         pprint(audio_features_vectors)
-        return audio_features_vectors
+        return np.array([vec for vec in audio_features_vectors])
 
     def create_playlist(self, name, description):
         """Creates a playlist. Requires scope playlist-modify-public."""
@@ -173,6 +196,7 @@ class SpotifyClient:
                    "public": True}
         self._data = json.dumps(request)
         response = self._post_api_data(endpoint)
+        check_api_response(response)
         pprint(response)
         return response
 
@@ -182,6 +206,7 @@ class SpotifyClient:
         self._headers['Content-Type'] = 'application/json'
         self._data = json.dumps([f'spotify:track:{track_id}' for track_id in track_ids])
         response = self._post_api_data(endpoint)
+        check_api_response(response)
         pprint(response)
         return response
 
@@ -204,12 +229,13 @@ if __name__ == '__main__':
     # mySpotify.get_top('artists', 'medium_term')
     # mySpotify.get_top('tracks', 'medium_term')
     # mySpotify.get_available_genre_seeds()
-    mySpotify.get_saved_tracks()
+    # mySpotify.get_saved_tracks(limit=2)
     # mySpotify.get_audio_features_of_currently_playing_track()
-    # mySpotify.get_audio_features_of_top_tracks()
+    audio_array = mySpotify.get_audio_features_of_top_tracks()
     # mySpotify.create_playlist("autogen2 playlist", "a new playlist")
     # mySpotify.add_tracks_to_playlist()
-    # mySpotify.create_playlist_of_top_tracks('medium_term')
+    # mySpotify.create_playlist_of_top_tracks('short_term')
+    compute_similarity_matrix(audio_array)
 
     # idea: use cosine similarity on artist genres to find similar artsists
         # Make playlist based on two or more peoples common genre interests
@@ -224,3 +250,5 @@ if __name__ == '__main__':
     # TODO: Try recommendations endpoint
     # TODO: create track subclass
     # Use liveness metrix to make playlist of live music
+
+    # Reorder playlist e- in ascending energy order?
