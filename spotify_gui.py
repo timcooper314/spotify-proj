f89e84869dd2f9d67693ab97058239eea7010007
Tim Cooper <timcooper314@gmail.com>
2021-07-11 14:20:51 +1000

    Converted get-current gui section to get-audio-features section. Added spotify client method for getting track details from track id. Made some methods protected with _ prefix.

--- spotify_gui.py
+++ spotify_gui.py
@@ -1,4 +1,5 @@
 import sys
+from pprint import pprint
 from PyQt5.QtWidgets import QApplication, QHBoxLayout, QVBoxLayout, QGroupBox, QGridLayout,\
     QPushButton, QRadioButton, QLineEdit, QDialog, QTableWidget, QTableView, QTextBrowser, QButtonGroup, QWidget
 from PyQt5.QtCore import QAbstractTableModel
@@ -14,7 +15,7 @@ class SpotifyGUI(QDialog):
         self.window_layout = QVBoxLayout()
         self.create_horizontal_get_top_layout()
         # self.create_results_table()
-        self.create_horizontal_get_current_layout()
+        self.create_horizontal_get_af_layout()
 
         self.window_layout.addWidget(self.horizontal_group_box)
         # self.window_layout.addWidget(self.results_table_box)
@@ -53,9 +54,9 @@ class SpotifyGUI(QDialog):
         h_layout.addLayout(top_type_buttons_layout)
 
         push_buttons_layout = QVBoxLayout()
-        start_button = QPushButton("Get data", self)
-        start_button.clicked.connect(self.get_top_click)
-        push_buttons_layout.addWidget(start_button)
+        get_top_data_button = QPushButton("Get data", self)
+        get_top_data_button.clicked.connect(self.get_top_click)
+        push_buttons_layout.addWidget(get_top_data_button)
 
         create_playlist_button = QPushButton("Create playlist", self)
         create_playlist_button.clicked.connect(self.create_playlist_click)
@@ -64,14 +65,23 @@ class SpotifyGUI(QDialog):
 
         self.horizontal_group_box.setLayout(h_layout)
 
-    def create_horizontal_get_current_layout(self):
-        self.horizontal_group_box_2 = QGroupBox("Current")
+    def create_horizontal_get_af_layout(self):
+        self.horizontal_group_box_2 = QGroupBox("Audio features")
         layout = QHBoxLayout()
 
-        start_button = QPushButton("Get current playing track", self)
-        start_button.clicked.connect(lambda: SpotifyClient().get_current_playback())
-        layout.addWidget(start_button)
+        v_layout = QVBoxLayout()
+        self.track_link_text = QLineEdit("Track link/ID", self)
+        v_layout.addWidget(self.track_link_text)
 
+        get_current_track_button = QPushButton("Get current playing track", self)
+        get_current_track_button.clicked.connect(self.get_current_click)
+        v_layout.addWidget(get_current_track_button)
+
+        layout.addLayout(v_layout)
+
+        get_current_af_button = QPushButton("Get audio features of track", self)
+        get_current_af_button.clicked.connect(self.get_af_click)
+        layout.addWidget(get_current_af_button)
         self.horizontal_group_box_2.setLayout(layout)
 
     # def create_results_table(self):
@@ -104,6 +114,26 @@ class SpotifyGUI(QDialog):
         number_tracks = int(self.number_tracks_text.text())
         time_period = self.get_time_period()
         create_playlist_of_top_tracks(time_range=time_period, limit=number_tracks)
+
+    def get_current_click(self):
+        current_data = SpotifyClient().get_current_playback()
+        current_id = current_data['item']['id']
+        self.track_link_text.setText(current_id)
+
+    def get_af_click(self):
+        track_link_or_id = self.track_link_text.text()
+        if track_link_or_id[0:4] == 'http':  # link
+            track_id = (track_link_or_id.split('/'))[4].split('?')[0]
+            SpotifyClient().get_track_from_id(track_id)
+        else:
+            track_id = track_link_or_id
+        af = SpotifyClient().get_audio_features(track_id)
+        pprint(af)
+
+    # TODO: Playlist analysis section...
+    # Input playlist id/link...
+    # Create playlist df, with audio features..
+    # Display as pd df, sortable by column...
 
 
 if __name__ == '__main__':
